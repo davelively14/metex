2a9db1abc3d59c529cd01315a63fc5d0b11c2efc
Dave Lively <davelively14@gmail.com>
2016-09-27 11:37:27 -0400

    Added worker.ex

---
+++ lib/worker.ex
@@ -0,0 +1,67 @@
+defmodule Metex.Worker do
+  use GenServer
+
+  # Client API
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  def get_temperature(pid, location) do
+    GenServer.call(pid, {:location, location})
+  end
+
+  # Server API
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  def handle_call({:location, location}, _from, stats) do
+    case temperature_of(location) do
+      {:ok, temp} ->
+        new_stats = update_stats(stats, location)
+        {:reply, "#{temp}°C", new_stats}
+      _ ->
+        {:reply, :error, stats}
+    end
+  end
+
+  # Helper Functions
+  defp temperature_of(location) do
+    location |> url_for |> HTTPoison.get |> parse_response
+  end
+
+  defp url_for(location) do
+    location = location |> URI.encode
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&APPID=#{apikey}"
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body |> JSON.decode! |> compute_temperature
+  end
+
+  defp parse_response(_) do
+    :error
+  end
+
+  defp compute_temperature(json) do
+    try do
+      temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+      {:ok, temp}
+    rescue
+      _ -> :error
+    end
+  end
+
+  defp apikey do
+    "4df888943ca2a1075ff4d686f0720016"
+  end
+
+  defp update_stats(old_stats, location) do
+    case Map.has_key?(old_stats, location) do
+      true ->
+        Map.update!(old_stats, location, &(&1 + 1))
+      false ->
+        Map.put_new(old_stats, location, 1)
+    end
+  end
+end
